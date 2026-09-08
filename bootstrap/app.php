02911a65b96c1d6634a25dc1e9602b56e30ea872
Restu Putra Anggara <restuwendi2@gmail.com>
2026-09-08 21:04:30 +0700

    fix(middleware): exempt api/* from CSRF validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
